3d5fe430d39abd82b45241da2656b7728374490a
coderjoshua800 <koladefreeman@gmail.com>
2026-03-14 09:51:48 -0700

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,11 @@
+#input a word 
+text = str(input("enter a string:  "))
+
+
+# reverse string 
+# using stop value as -1 to iterate in reverse  
+revtext = text[::-1]
+text = revtext
+
+print("reverse of given string is:")
+print(text)
